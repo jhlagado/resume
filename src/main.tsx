e2547ed2161e32e7f017de55928a36c6d81a5e29
John Hardy <jh@lagado.com>
2024-02-13 13:13:23 +1100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Resume } from "./resume";
+import { Resume } from "./Resume";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
